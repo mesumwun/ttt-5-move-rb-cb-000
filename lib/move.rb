25d7173f3f9e868b47df85fd8e174d1fadd5a5c2
Daniel Hope <upinhope@gmail.com>
2018-01-29 21:46:43 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+# Create the Tic Tac Toe board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# code to get user input and convert to int
+def input_to_index(user_input)
+  converted_input = user_input.to_i - 1
+end
+
+# code to create array index and value
+def move(board, index, value = "X")
+  return board[index] = value
+end
